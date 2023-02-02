b81cfe817debb2ab5186165d29c72e09d9b156ab
Eline31 <eline.chambon@gmail.com>
2023-02-02 15:07:33 +0100

    Récupération de l'id du product à afficher

--- front/js/product.js
+++ front/js/product.js
@@ -1,22 +1,30 @@
 //Il me faut une fonction qui lise l'URL pour repérer l'id et afficher
+//Je récupère l'url courante
+const url = new URL(document.location);
+//La propriété searchParams de url retourne un objet de type "URLSearchParams"
+const searchParams = url.searchParams;
+//Je récupère l'id du produit déjà présent dans l'URL
+const id = searchParams.get("id");
+
+
 //les informations du produit en conséquence
 //Création de la fonction URL
-function productPage(product_id) {
+function productPage(id) {
     const productDetails = document.querySelector(".item");
     const imageUrlElement = document.querySelector(".item_img");
-    imageUrlElement.src = product.imageUrl;
-    imageUrlElement.alt = product.altTxt;
+    imageUrlElement.src = products.imageUrl;
+    imageUrlElement.alt = products.altTxt;
     const productContentElement = document.querySelector(".item_content");
     const titlePriceElement = document.querySelector(".item_content_titlePrice");
     const nameElement = document.querySelector("#title");
-    nameElement.innerText = product.name;
+    nameElement.innerText = products.name;
     const priceElement = document.querySelector("#price");
-    priceElement.innerText = product.name;
+    priceElement.innerText = products.name;
     const productDescriptionElement = document.querySelector(".item_content_description");
     const descriptionElement = document.querySelector("#description");
-    descriptionElement.innerText = product.description;
+    descriptionElement.innerText = products.description;
     const ColorsElement = document.querySelector("#colors");
-    ColorsElement.innerText = product.colors;
+    ColorsElement.innerText = products.colors;
 
     productDetails.appendChild(imageUrlElement);
     productDetails.appendChild(productContentElement);
@@ -30,19 +38,19 @@ function productPage(product_id) {
 };
 
 async function displayProductPage() {
-    const result = await fetch("http://localhost:3000/api/products/");
+    const result = await fetch(`http://localhost:3000/api/products/${id}`);
     const products = await result.json();
-    const productDetails = document.querySelector(".item");
-    //Je récupère l'url courante
-    const url = new URL(document.location);
-    //La propriété searchParams de url retourne un objet de type "URLSearchParams"
-    const searchParams = url.searchParams;
-    //Je récupère l'id du produit déjà présent dans l'URL
-    searchParams.get("id");
-    for (let product of products) {
-        const _productPage = productPage(product);
-        productDetails.appendChild(_productPage);
-    }
+    //const productDetails = document.querySelector(".item");
+    //productDetails.appendChild(_productPage);
+    return productPage(id);
 };
 
-//displayProductPage();
+
+
+/*for (let product of products) {
+    const _productPage = productPage(id);
+    productDetails.appendChild(_productPage);
+};
+};
+
+displayProductPage();*/
