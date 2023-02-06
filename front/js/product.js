d3996a3f7c7dff60ef0b06e6b6a807ba3ac7246b
Eline31 <eline.chambon@gmail.com>
2023-02-06 18:40:09 +0100

    Page produit fonctionnelle

--- front/js/product.js
+++ front/js/product.js
@@ -1,59 +1,40 @@
-//Il me faut une fonction qui lise l'URL pour repérer l'id et afficher
-//Je récupère l'url courante
+//Je récupère l'url de la page courante
 const url = new URL(document.location);
-//La propriété searchParams de url retourne un objet de type "URLSearchParams"
+//La propriété searchParams de l'url retourne un objet de type "URLSearchParams"
 const searchParams = url.searchParams;
 //Je récupère l'id du produit déjà présent dans l'URL
 const productId = searchParams.get("id");
 
-//les informations du produit en conséquence
-//Création de la fonction URL
-function showProduct(product) {
-    const productDetails = document.querySelector(".item");
-    const imageUrlContent = document.querySelector(".item_img");
+//Fonction permettant d'ajouter les infos produit au DOM
+function addProductDetails(product) {
+    const imageUrlContent = document.querySelector(".item__img");
     const imageUrlElement = document.createElement("img");
     imageUrlElement.src = product.imageUrl;
     imageUrlElement.alt = product.altTxt;
-    console.log(imageUrlElement); // ça fonctionne, je ne comprends pas le pbm d'affichage
-    const productContentElement = document.querySelector(".item_content");
-    const titlePriceElement = document.querySelector(".item_content_titlePrice");
-    const nameElement = document.querySelector("#title");
+    const nameElement = document.getElementById("title");
     nameElement.innerText = product.name;
-    const priceElement = document.querySelector("#price");
+    const priceElement = document.getElementById("price");
     priceElement.innerText = product.price;
-    //const productDescriptionElement = document.querySelector(".item_content_description");
-    const descriptionElement = document.querySelector("#description");
+    const descriptionElement = document.getElementById("description");
     descriptionElement.innerText = product.description;
-    const colorsContent = document.querySelector("#colors");
+    const colorsContent = document.getElementById("colors");
     const colors = product.colors;
+    //Création d'une fonction for pour afficher les éléments colors de l'API
     for (let i = 0; i < colors.length; i++) {
         const colorElement = document.createElement("option");
         colorElement.innerText = colors[i];
         colorsContent.appendChild(colorElement);
-    };//Attention, apparaît en double !!
-
-    //productDetails.appendChild(imageUrlElement);
-    productDetails.appendChild(imageUrlContent);
+    };
     imageUrlContent.appendChild(imageUrlElement);
-    productDetails.appendChild(productContentElement);
-    productContentElement.appendChild(titlePriceElement);
-    titlePriceElement.appendChild(nameElement);
-    titlePriceElement.appendChild(priceElement);
-    productDetails.appendChild(descriptionElement);
-    productDetails.appendChild(colorsContent);
-
-    return productDetails;
+    colorsContent.appendChild(colors);
 };
 
-//Vaut-il mieux mettre des .then et .catch ??
-/* const result = () => fetch(`http://localhost:3000/api/products/${productId}`)
-    .then (product => result.jason());
-    .catch(err => {});*/
+//Appel à l'API pour les infos du produit cliqué
 async function getProduct() {
     const result = await fetch(`http://localhost:3000/api/products/${productId}`);
     const product = await result.json();
-    //const productDetails = document.querySelector(".item");
-    return showProduct(product);
+
+    addProductDetails(product);
 };
 
 getProduct();
